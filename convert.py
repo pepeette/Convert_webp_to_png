a5ad3c1511f28d22aa8cd38dee269db0fdbadbcc
pepeette <59664130+pepeette@users.noreply.github.com>
2023-02-08 16:11:08 +0700

    create convert.py

---
+++ convert.py
@@ -0,0 +1,11 @@
+!pip install webptools
+from webptools import dwebp
+import os, glob
+os.chdir("./Product Group images")
+
+webp_list = glob.glob("*.webp")
+
+for filename in webp_list:
+    #print(filename[:-4]+"png")
+    outname = (filename[:-4]+"png")
+    dwebp(input_image=filename, output_image=outname, option="-o", logging="-v")
